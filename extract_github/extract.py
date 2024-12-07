d59b5e33e1a1d8658e653f12b4d334b274c176bf
sunny2g <sam25896@gmail.com>
2024-12-07 22:29:13 +0530

    fixed a bug and moved the function calculate_streak to func file

--- extract_github/extract.py
+++ extract_github/extract.py
@@ -1,15 +1,16 @@
 
 
-import requests
+
 from datetime import datetime, timedelta 
 from config.env_variables import USERNAME
-from include.func_file import fetch_github_events
+from include.func_file import fetch_github_events, calculate_streak
 import json
 
 ## Improvements
 # Send daily mail with the streak count 
 # Send more information like commit info project name timestamp etc.
 
+# fetch_github_events fuction will try to connect with github and login 
 events = fetch_github_events(USERNAME)
 
 ## Added the code to save json data in file.
@@ -19,40 +20,7 @@ with open(file_path, 'w') as file:
     json.dump(events, file, indent=4) 
     print(f"Data has been written to {file_path}")
 
-## print(events)
 
-
-# Function to process events and calculate streaks 
-
-def calculate_streak(events): 
-    contributions = []
-
-    # Extract only the dates of contributions
-    for event in events :
-        if 'created_at' in events: 
-            contribution_date = event['created_at'][:10] # Extract date (YYYY-MM-DD)
-            contributions.append(contribution_date)
-
-    # Sort the dates in ascending order 
-    contributions = sorted(set(contributions))
-
-    # Calculate streaks 
-    streaks = []
-    current_streak = 1
-
-    for i in range(1, len(contributions)) :
-        current_date = datetime.strftime(contributions[i],"%Y-%m-%d")
-        previous_date = datetime.strftime(contributions[i - 1],"%Y-%m-%d")
-        if current_date - previous_date == timedelta(days=1) :
-            current_streak += 1 
-        else :
-            streaks.append(current_streak)
-            current_streak = 1
-
-    # Append the last streak 
-    streaks.append(current_streak)
-
-    return streaks
 
 # Calculate streaks from the fetched events 
 streaks = calculate_streak(events)
@@ -60,10 +28,10 @@ streaks = calculate_streak(events)
 # Print the streaks 
 print("User's Streak (in Days ): " , streaks) 
 
-
 # Adding Mailing functionality
 # SMTP server settings for Gmail
 subject = "Github_Streak_Mails"
 body = f"User's Streak (in Days ): {streaks}"
 from include.func_file import mail_user
 mail_user(streaks,subject,body)
+
--- extract_github/include/func_file.py
+++ extract_github/include/func_file.py
@@ -1,6 +1,6 @@
 import requests
 from config.env_variables import GITHUB_API_URL,USERNAME,TOKEN
-
+from datetime import datetime, timedelta 
 
 def mail_user(streaks,subject,body) :
     from config.env_variables import sender_email,receiver_email,app_password
@@ -40,3 +40,47 @@ def fetch_github_events(username):
     else:
         print("Failed to fetch events data: ", response.status_code)
         return []
+    
+
+# Function to process events and calculate streaks 
+def calculate_streak(events): 
+    contributions = []
+
+    # Extract only the dates of contributions
+    for event in events :
+        if 'created_at' in event:
+            contribution_date = event['created_at'][:10] # Extract date (YYYY-MM-DD)
+            contributions.append(contribution_date)
+    
+    contrib_converted=[]
+    for dt in contributions:
+        converted_dt=datetime.strptime(dt, "%Y-%m-%d")
+        converted_dt=converted_dt.date()
+        contrib_converted.append(converted_dt)
+
+    # Sort the dates in ascending order 
+    contributions = sorted(set(contrib_converted))
+
+    # Calculate streaks 
+    streaks = []
+    current_streak = 1
+    
+
+    for i in range(1, len(contributions)) :
+        #current_date = datetime.strftime(contributions[i],"%Y-%m-%d")
+        #current_date=current_date.date()
+        #previous_date = datetime.strftime(contributions[i - 1],"%Y-%m-%d")
+        #previous_date=previous_date.date()
+        
+        current_date = contributions[i]
+        previous_date = contributions[i - 1]
+       
+        if current_date - previous_date == timedelta(days=1) :
+            current_streak += 1 
+        else :
+            streaks.append(current_streak)
+            current_streak = 1
+
+    # Append the last streak 
+    streaks.append(current_streak)
+    return streaks
